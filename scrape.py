f896d99e4593cf9b87c4011c5e910c434203e7b4
Nadun Perera <nudie.perera@gmail.com>
2019-05-12 12:34:52 +1000

    added some comments to scrape.py

--- scrape.py
+++ scrape.py
@@ -1,6 +1,13 @@
 from spiders import woolworths
 
-chrome_path = r"C:\Users\nadun\source\repos\ghn_scraper_service\chromedriver_win32\chromedriver.exe"
+chrome_path = r'C:\Users\nadun\source\repos\ghn_scraper_service\chromedriver_win32\chromedriver.exe'
+
+# Do not use the url with the page number.
+# Make sure it is clean as below. Spider will be adding the pageNumber potion while crawling.
+# SAMPLE URLS
+# https://www.woolworths.com.au/shop/browse/fruit-veg/fruit
+# https://www.woolworths.com.au/shop/browse/drinks/soft-drinks
+
 url = 'https://www.woolworths.com.au/shop/browse/dairy-eggs-fridge/cheese'
 
 woolworths.crawl(chrome_path, url)
