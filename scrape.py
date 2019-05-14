6c97b889a1e6a8c815232d1eb998c4b0f7f71ead
sugeivan-accura <sugeivan@accura-tech.com>
2019-05-14 17:21:44 +0530

    scraping from categories

--- scrape.py
+++ scrape.py
@@ -10,8 +10,8 @@ chrome_path = r'src/chromedriver_win32/chromedriver.exe'
 # https://www.woolworths.com.au/shop/browse/fruit-veg/fruit
 # https://www.woolworths.com.au/shop/browse/drinks/soft-drinks
 
-scrape_url = 'https://www.woolworths.com.au/shop/browse/dairy-eggs-fridge/cheese'
-scrape_url = 'https://www.jbhifi.com.au/computers-tablets/hp-laptops/'
+# scrape_url = 'https://www.woolworths.com.au/shop/browse/dairy-eggs-fridge/cheese'
+scrape_url = 'https://www.jbhifi.com.au/computers-tablets/' #put one category url
 
 # setting up the browser
 browser = chrome.Browser(chrome_path)
--- src/spiders/jbhifi.py
+++ src/spiders/jbhifi.py
@@ -5,18 +5,17 @@ from selenium.common.exceptions import NoSuchElementException
 
 
 def crawl(driver, scrape_url):
-
-    page_number = 1
-    total_products = 0
-    products = get_product_details(scrape_url, 1, driver)
+    category_urls = get_category_details(scrape_url, driver)
 
     # output to a file
     with open('crawl_results.csv', 'w', newline='') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(['Product Name', 'Price'])
 
-        while products:
-            # write to the file whatever you get from crawling
+        for category_url in category_urls:
+            total_products = 0
+            page_number = 1
+            products = get_product_details(category_url, page_number, driver)
             for product in products:
                 try:
                     writer.writerow([product.find_element_by_tag_name('h4').text,
@@ -26,16 +25,32 @@ def crawl(driver, scrape_url):
 
             total_products = total_products + len(products)
             page_number = page_number + 1
-            products = get_product_details(scrape_url, page_number, driver)
+            products = get_product_details(category_url, page_number, driver)
 
             if not products:
-                print(f'Total number of products crawled: {total_products}')
+                page_number = 0
+                print(f'Total number of  category products crawled: {total_products}')
                 break
 
 
 def get_next_page(scrape_url, page_number):
     scrape_url = scrape_url + f'?p={page_number}'
     return scrape_url
+
+
+def get_category_details(scrape_url, driver):
+    driver.get(scrape_url)
+    time.sleep(2)
+    categories = driver.find_element_by_class_name('category-menu')
+    category_urls = []
+    for li in categories.find_elements_by_tag_name("li"):
+        try:
+            url = li.find_element_by_tag_name('a').get_attribute('href')
+            if url[-1] != '#':
+                category_urls.append( url)
+        except NoSuchElementException:
+            pass  # passing if unable to locate above elements
+    return category_urls
 
 
 def get_product_details(scrape_url, page_number, driver):
