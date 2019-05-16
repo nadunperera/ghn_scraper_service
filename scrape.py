ec7bfac1364ac8845a8d30f3ddc1e86fc9874644
Nadun Perera <nadun.perera@me.com>
2019-05-17 09:31:39 +1000

    cleaned up the code a bit

--- scrape.py
+++ scrape.py
@@ -1,23 +1,21 @@
 from src.spiders import woolworths, jbhifi
 from src.helpers import pandas, chrome
 
-chrome_path = r'src/chromedriver_win32/chromedriver.exe'
-
 # Do not use the url with the page number.
 # Make sure it is clean as below. Spider will be adding the pageNumber potion while crawling.
 # SAMPLE URLS
 # https://www.woolworths.com.au/shop/browse/fruit-veg/fruit
 # https://www.woolworths.com.au/shop/browse/drinks/soft-drinks
 
-scrape_url = 'https://www.woolworths.com.au/shop/browse/dairy-eggs-fridge/cheese'
+scrape_url = "https://www.woolworths.com.au/shop/browse/dairy-eggs-fridge/cheese"
 # scrape_url = 'https://www.jbhifi.com.au/computers-tablets/' #put one category url
 
 # setting up the browser
-browser = chrome.Browser(chrome_path)
+browser = chrome.Browser()
 
 # do the crawl
-jbhifi.crawl(browser.driver, scrape_url)
-# woolworths.crawl(browser.driver, scrape_url)
+# jbhifi.crawl(browser.driver, scrape_url)
+woolworths.crawl(browser.driver, scrape_url)
 
 # distinct results
 pandas.distinct()
--- src/helpers/chrome.py
+++ src/helpers/chrome.py
@@ -3,14 +3,10 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 class Browser:
-    def __init__(self, browser_path):
-        self.path = browser_path
-
     @property
     def driver(self):
-        # options_headless = webdriver.ChromeOptions()
-        # options_headless.add_argument("headless")
-        driver = webdriver.Remote("http://127.0.0.1:4444/wd/hub", DesiredCapabilities.CHROME)
-        # driver = webdriver.Chrome(self.path)
+        driver = webdriver.Remote(
+            "http://127.0.0.1:4444/wd/hub", DesiredCapabilities.CHROME
+        )
         print("Chrome driver initialised.")
         return driver
