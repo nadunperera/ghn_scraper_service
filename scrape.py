4855591b9abbe4c054d18b8575a934244da4cf2d
Nadun Perera <nadun.perera@me.com>
2019-05-18 18:28:04 +1000

    added todo.txt, and data folder to hold JSON files

--- scrape.py
+++ scrape.py
@@ -7,9 +7,9 @@ from src.helpers import pandas, browse
 # https://www.woolworths.com.au/shop/browse/fruit-veg/fruit
 # https://www.woolworths.com.au/shop/browse/drinks/soft-drinks
 
-# scrape_url = "https://www.woolworths.com.au/shop/browse/dairy-eggs-fridge/cheese"
+scrape_url = "https://www.woolworths.com.au/shop/browse/dairy-eggs-fridge/cheese"
 # scrape_url = "https://www.jbhifi.com.au/computers-tablets/" #put one category url
-scrape_url = "https://shop.coles.com.au/a/a-national/everything/browse/entertaining-at-home/cheese-board-selections?pageNumber=1"
+# scrape_url = "https://shop.coles.com.au/a/a-national/everything/browse/entertaining-at-home/cheese-board-selections?pageNumber=1"
 
 # select the browser driver to use
 # chrome_driver for chrome
@@ -18,8 +18,8 @@ browser = browse.Browser()
 
 # do the crawl
 # jbhifi.crawl(browser.chrome_driver, scrape_url)
-# woolworths.crawl(browser.phantomjs_driver, scrape_url)
-coles.crawl(browser.phantomjs_driver, scrape_url)
+woolworths.crawl(browser.chrome_driver, scrape_url)
+# coles.crawl(browser.chrome_driver, scrape_url)
 
 # distinct results
 pandas.distinct()
